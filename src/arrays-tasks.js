54f5066ea9f1843d2bb9b669be272e07ff4845e5
Sirius Black <an.kozimirov@gmail.com>
2024-02-10 11:14:40 +0300

    feat: implement shiftArray function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -566,8 +566,18 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  if (n > 0) {
+    const rightPart = arr.slice(-n);
+    const leftPart = arr.slice(0, -n);
+    return rightPart.concat(leftPart);
+  }
+  if (n < 0) {
+    const leftPart = arr.slice(-n, arr.length);
+    const rightPart = arr.slice(0, -n);
+    return leftPart.concat(rightPart);
+  }
+  return arr.slice();
 }
 
 /**
